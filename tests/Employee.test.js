9cfae9910849a4aa4dfdee3d580f9b33b3254ba2
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-24 20:56:39 -0400

    started employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,4 +1,3 @@
-const { identity } = require("rxjs")
 
 // name
 // id
@@ -7,3 +6,51 @@ const { identity } = require("rxjs")
 // getId()
 // getEmail()
 // getRole() - returns "employee"
+
+const Employee = require("../lib/Employee.js");
+
+describe("Employee", () => {
+  describe("Initialization", () => {
+    it("should create an object with a name, age and id if provided valid arguments", () => {
+      const employee = new Employee("Eileen", "eileenrdolan@gmail.com", "001");
+
+      expect(employee.name).toEqual("Sarah");
+      expect(employee.age).toEqual(3);
+    });
+
+    it("should throw an error if provided no arguments", () => {
+      const cb = () => new Employee();
+
+      expect(cb).toThrow();
+    });
+
+    it("should throw an error if not provided an age", () => {
+      const cb = () => new Employee("Sarah");
+      const err = new Error("Expected parameter 'age' to be a non-negative number");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if 'name' is not a string", () => {
+      const cb = () => new Employee(3, 2);
+      const err = new Error("Expected parameter 'name' to be a non-empty string");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if 'age' is not a number", () => {
+      const cb = () => new Employee("Sarah", "2");
+      const err = new Error("Expected parameter 'age' to be a non-negative number");
+
+      expect(cb).toThrowError(err);
+    });
+
+    it("should throw an error if 'age' is less than 0", () => {
+      const cb = () => new Employee("Sarah", -1);
+      const err = new Error("Expected parameter 'age' to be a non-negative number");
+
+      expect(cb).toThrowError(err);
+    });
+  });
+});
+

0f7ce24b2df6acc4eb313f65650a4a8030c79710
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-25 21:16:00 -0400

    worked on promptList.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 
+constructor()
 this.employees
 this.html
 
 this.employees.push(new Manager)
+
+fs.writeFile('./dist/index.html', team.getHTML(), (err=> {
+    if (err)
+        console.log(err);
+    else ;    
+}))
---
+++ src/promptList.js
@@ -0,0 +1,15 @@
+//Inquirer Library
+const inquirer = require('inquirer');
+
+const promptList = function (message, choices){
+    return inquirer.prompt([{
+        name: "val",
+        type: "list",
+        message: message,
+        choices: choices,
+        pageSize: 10,
+    }])
+    .then(answer => answer.val);
+}
+
+module.exports = promptList;

626502100540e8963c81b5a56f350d90a98b0b7a
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-10-29 18:59:12 -0400

    updating index.js and readme

--- index.js
+++ index.js
@@ -13,29 +13,55 @@ async function buildTeam(){
 
     const managerName = await promptInput("Please Enter the Manager's Name:", true), 
     const managerEmail = await promptInput("Please Enter the Manager's Name:", true); 
+    const managerId = await promptInput("Please Enter the Manager's ID #:", true);
     const managerOfficeNumber = await promptInput("Please Enter the Manager's Name:", true); 
 
-    const manager = new Manager(managerName, managerEmail, managerOfficeNumber);
+    const manager = new Manager(managerName, managerEmail, managerId, managerOfficeNumber);
      
     employees.push(manager);
 
     let choice = '';
-    while (choice !== 'Finished'){
-            choice = await promptList("\nWhich team member would you like to add to the team?",   ['Engineer', 'Intern', 'Finished']);
+    while (choice !== 'Team Complete'){
+            choice = 
+            ("\nWhich team member would you like to add to the team?", ['Engineer', 'Intern', 'Team Complete']);
 
             if (choice === 'Engineer'){
-                const engineerName = await promptInput("Please Enter the Manager's Name:", true), 
-                const engineerEmail = await promptInput("Please Enter the Manager's Name:", true); 
-                const engineerGitHub = await promptInput("Please Enter the Manager's Name:", true); 
+                const engineerName = await promptInput("Please Enter the Engineer's Name:", true), 
+                const engineerEmail = await promptInput("Please Enter the Engineer's Name:", true); 
+                const engineerId = await promptInput("Please Enter the Engineers's ID #:", true);
+                const engineerGitHub = await promptInput("Please Enter the Engineer's Name:", true); 
 
-                const engineer = new Engineer(engineerName, managerEmail, engineerGitHub);
+                const engineer = new Engineer(engineerName, engineerEmail, engineerId, engineerGitHub);
 
                 employees.push(engineer);
+            } 
+
+            if (choice === 'Intern'){
+                const internName = await promptInput("Please Enter Intern's Name:", true), 
+                const internEmail = await promptInput("Please Enter the Intern's Name:", true); 
+                const internId = await promptInput("Please Enter the Intern's ID #:", true);
+                const internSchool = await promptInput("Please Enter the Intern's Name:", true); 
+
+                const intern = new Intern(internName, internEmail, internId, internSchool);
+
+                employees.push(intern);
             } 
         }
 
     // generate HTML string (HTML) with data from employees array    
-    let htmlString = '!<DOCTypeslkdlskdlsklds';
+    let htmlString = "<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>;
+
     for(const employee of employees){
         const role = employee.getRole(); 
         // generate html card

de0bfb2c0278fe7e88b5e96b853bfb46693e09e9
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-11-01 18:31:05 -0400

    started a generateHTML page

--- index.js
+++ index.js
@@ -131,48 +131,14 @@ function addIntern(){
    // generate HTML string (HTML) with data from employees array  
    
    function generateHTML(){
-    let htmlString = `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Team Profile Generator</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>`
+    fs.writeFileSync("dist/index.html", generateHTML(team))
+   }
 
 
-    for(const employee of team){
-        const role = employee.getRole(); 
-        // generate html card
+    // for(const employee of team){
+    //     const role = employee.getRole(); 
+    //     // generate html card
 
-        htmlStr+=
-                `<div class="card has-background-white-ter">
-                    <header class="card-header is-flex is-flex-direction-column has-background-info pl-3">
-                        <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getName()}</h2>
-                        <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getName()}</h2>
-                        <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getName()}</h2>
-                        `
-        // always display id, name, email
-        
-        // if role = manager
-        //    display coffeee mug icon <i class="fa-solid fa-mug-hot"></i>
-        //    display office number
-    
-        // if role = engineer
-        //    display glasses icon <i class="fa-solid fa-glasses-round"></i>
-        //    display github username, which is link to github page https://www.github.com/username
-        
-        // if role = intern
-        //    display scholar icon <i class="fa-solid fa-user-graduate"></i>
-        //    display school
-
-
-
-    }
+   
   
-  fs.writeFileSync("dist/index.html", htmlString)
-}
+  

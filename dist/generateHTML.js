fed74602eb0ead6aefd0a56fb3a097e6ed150806
ereneedolan <112826913+ereneedolan@users.noreply.github.com>
2022-12-10 15:42:21 -0500

    finished getting HTML to generate

--- dist/generateHTML.js
+++ dist/generateHTML.js
@@ -3,14 +3,29 @@ let employeeList = ''
 
 for (let i=0; i<team.length; i++){
     let employee = team[i]
+    let lastH2 = ''
+    if (employee.getRole() === 'Manager') {
+        lastH2= employee.officeNumber
+    } 
+
+    else if (employee.getRole()=== 'Engineer') {
+        lastH2= employee.github
+    }
+
+    else if (employee.getRole() === 'Intern'){
+        lastH2= employee.school
+    }
     let employeeCard = `<div class="card has-background-white-ter">
     <header class="card-header is-flex is-flex-direction-column has-background-info pl-3">
         <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getName()}</h2>
         <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getEmail()}</h2>
         <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${employee.getId()}</h2>
+        <h2 class="card-header-title has-text-white is-size-3 pb-0 ">${lastH2}</h2>
     </header>
     </div>   
-    `    
+    `  
+    
+
 employeeList = employeeList + employeeCard 
 }
 
